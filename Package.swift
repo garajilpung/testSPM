1e30caee1ce73a91565336b86300028b5ca14f80
Ha Seock Seong <>
2023-08-08 15:05:33 +0900

    asdf

--- Package.swift
+++ Package.swift
@@ -20,7 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "testSPM",
-            dependencies: []),
+            dependencies: [],
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        ),
         .testTarget(
             name: "testSPMTests",
             dependencies: ["testSPM"]),
